8cddc6777fb1bcbfbca536b2788742bf2b2bb671
kphed <kpcrypto69@gmail.com>
2023-10-16 13:00:28 -0400

    Configure linting and formatter rules

--- src/getOwners.js
+++ src/getOwners.js
@@ -1,43 +1,43 @@
-const fs = require('fs');
-const { RUMAS, FILEPATHS } = require('./constants');
+const fs = require("fs");
+const { RUMAS, FILEPATHS } = require("./constants");
 
 module.exports = async (contractInstance) => {
-  const owners = {};
+    const owners = {};
 
-  for (let i = 1; i <= RUMAS.LAST_TOKEN_ID; ++i) {
-    let owner = await contractInstance.read.ownerOf({
-      args: [i],
-      blockNumber: RUMAS.CUTOFF_BLOCK_NUMBER,
-    });
+    for (let i = 1; i <= RUMAS.LAST_TOKEN_ID; ++i) {
+        let owner = await contractInstance.read.ownerOf({
+            args: [i],
+            blockNumber: RUMAS.CUTOFF_BLOCK_NUMBER,
+        });
 
-    // If the token was burned/refunded or redeemed, skip it (the latter was only done by our team).
-    if (
-      owner === '0x000000000000000000000000000000000000dEaD' ||
-      owner === '0x0000000000000000000000000000000000000000'
-    )
-      continue;
+        // If the token was burned/refunded or redeemed, skip it (the latter was only done by our team).
+        if (
+            owner === "0x000000000000000000000000000000000000dEaD" ||
+            owner === "0x0000000000000000000000000000000000000000"
+        )
+            continue;
 
-    // Get the actual owner if the token is listed (i.e. the Rumas contract is custodying it).
-    if (owner === RUMAS.CONTRACT_ADDRESS) {
-      owner = (
-        await contractInstance.read.listings({
-          args: [i],
-          blockNumber: RUMAS.CUTOFF_BLOCK_NUMBER,
-        })
-      )[0];
+        // Get the actual owner if the token is listed (i.e. the Rumas contract is custodying it).
+        if (owner === RUMAS.CONTRACT_ADDRESS) {
+            owner = (
+                await contractInstance.read.listings({
+                    args: [i],
+                    blockNumber: RUMAS.CUTOFF_BLOCK_NUMBER,
+                })
+            )[0];
+        }
+
+        if (!owners[owner]) {
+            owners[owner] = {
+                totalOwned: 1,
+                ownerOf: [i],
+            };
+        } else {
+            ++owners[owner].totalOwned;
+
+            owners[owner].ownerOf.push(i);
+        }
     }
 
-    if (!owners[owner]) {
-      owners[owner] = {
-        totalOwned: 1,
-        ownerOf: [i],
-      };
-    } else {
-      ++owners[owner].totalOwned;
-
-      owners[owner].ownerOf.push(i);
-    }
-  }
-
-  fs.writeFileSync(FILEPATHS.OWNERS, JSON.stringify(owners));
+    fs.writeFileSync(FILEPATHS.OWNERS, JSON.stringify(owners));
 };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { createPublicClient, getContract, http } = require('viem');
-const { mainnet } = require('viem/chains');
-const { RUMAS } = require('./constants');
-const collectionAbi = require('./abi/collection.json');
-const getOwners = require('./getOwners');
+const { createPublicClient, getContract, http } = require("viem");
+const { mainnet } = require("viem/chains");
+const { RUMAS } = require("./constants");
+const collectionAbi = require("./abi/collection.json");
+const getOwners = require("./getOwners");
 
 const client = createPublicClient({
-  chain: mainnet,
-  transport: http(process.env.RPC_URL),
-  batch: { multicall: true },
+    chain: mainnet,
+    transport: http(process.env.RPC_URL),
+    batch: { multicall: true },
 });
 
 const collectionContract = getContract({
-  address: RUMAS.CONTRACT_ADDRESS,
-  abi: collectionAbi,
-  publicClient: client,
+    address: RUMAS.CONTRACT_ADDRESS,
+    abi: collectionAbi,
+    publicClient: client,
 });
 
 // Retrieve the list of Rumas NFT token holders at the time of the snapshot (1 block after the mint ended).
