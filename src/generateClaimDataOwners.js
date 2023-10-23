cae7a009bd341b6a53001eb947cb0acbcadf3a34
kphed <kpcrypto69@gmail.com>
2023-10-23 11:32:31 -0400

    Compute tree and store root and proofs for owner claims

--- src/generateClaimDataOwners.js
+++ src/generateClaimDataOwners.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
+const { StandardMerkleTree } = require("@openzeppelin/merkle-tree");
 const owners = require("./data/owners.json");
 const { TOKEN_CLAIM_AMOUNTS, FILEPATHS } = require("./constants");
 
 (() => {
-    const ownerClaims = Object.keys(owners).reduce((ownerClaims, owner) => {
-        return [
+    const ownerClaims = Object.keys(owners).reduce(
+        (ownerClaims, owner) => [
             ...ownerClaims,
             [
                 owner,
@@ -12,8 +13,16 @@ const { TOKEN_CLAIM_AMOUNTS, FILEPATHS } = require("./constants");
                     BigInt(owners[owner].totalOwned) * TOKEN_CLAIM_AMOUNTS.OWNER
                 ).toString(),
             ],
-        ];
-    }, []);
+        ],
+        []
+    );
+    const tree = StandardMerkleTree.of(ownerClaims, ["address", "uint256"]);
+    const proofs = {};
 
-    fs.writeFileSync(FILEPATHS.CLAIM_DATA_OWNERS, JSON.stringify(ownerClaims));
+    for (const [i, v] of tree.entries()) proofs[v[0]] = tree.getProof(i);
+
+    fs.writeFileSync(
+        FILEPATHS.CLAIM_DATA_OWNERS,
+        JSON.stringify({ root: tree.root, proofs, data: ownerClaims })
+    );
 })();
